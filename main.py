7abce10d86e902648b600328abb2bd51ed135624
Yousef Hadhood <skillissuecmaster@gmail.com>
2026-08-04 17:56:17 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def main():
-    print("Hello from hezeny!")
-
+    print("Hello from hezeny! but i am an impostor main file, the real main file (for backend) is at `app/main.py`")
 
 if __name__ == "__main__":
     main()
